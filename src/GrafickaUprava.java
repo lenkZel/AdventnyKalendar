9c1499d7fa341c0c9ed437d3513f27a6fb4919ec
Student Java <l.zeliarova@gmail.com>
2024-02-22 16:49:23 +0100

    My first repository

--- src/GrafickaUprava.java
+++ src/GrafickaUprava.java
@@ -13,8 +13,7 @@ public class GrafickaUprava {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-
-            JFrame okno = new JFrame();  // Vytvorenie hlavného okna
+            JFrame okno = new JFrame();
             okno.setTitle("Adventný kalendár");
             okno.setSize(OKNO_SIRKA, OKNO_VYSKA);
             okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -24,16 +23,17 @@ public class GrafickaUprava {
 
             ArrayList<Boolean> otvoreneOkno = new ArrayList<>(24);
 
-            LocalDate aktualnyDatum = LocalDate.now();  // Nastavenie aktuálneho dátumu
+            LocalDate aktualnyDatum = LocalDate.now();
             int aktualnyDen = aktualnyDatum.getDayOfMonth();
 
             for (int den = 1; den <= 24; den++) {
                 boolean jeOtvorene = den <= aktualnyDen ? otvoreneOkno.add(true) : otvoreneOkno.add(false);
 
-                JButton tlacidlo = new JButton(String.valueOf(den));  // Vytvorenie tlacidla (okna)
+                JButton tlacidlo = new JButton(String.valueOf(den));
                 tlacidlo.setEnabled(otvoreneOkno.get(den - 1));
-                tlacidlo.setBackground(new Color(153,204,255));
+                tlacidlo.setBackground(new Color(153,255,51));
                 int finalDen = den;
+                //pridanie ActionListenera ku tlacidlu
                 tlacidlo.addActionListener(udalost -> {
                     if (otvoreneOkno.get(finalDen -1)){
                         String darcek = AdventnyKalendar.darcekDna(finalDen, random);
